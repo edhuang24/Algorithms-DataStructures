1996a33118cef199b98691c2c362c9e65e8c3848
edhuang24 <edhuang24@gmail.com>
2015-11-30 23:39:21 -0800

    finish linked list practice problem

--- algorithms/practice_problems.rb
+++ algorithms/practice_problems.rb
@@ -1378,3 +1378,52 @@ p permutations([1,2,3])
 # ...
 # 1 truck of fuel: 100/1 miles
 # Thus, you can then add up 100/50 + 100/49 + 100/48 + ... + 100/1. This is ~449.9.
+
+# **************************************************************************** #
+
+# cyclic
+
+# Consider a linked list. Each link in the list holds a next reference to the next item in the list, except for the final link, which points to nil.
+#
+# It is possible to have a "list" without any end, which loops back on itself. Possibilities:
+#
+# A -> B -> C -> A -> ...
+# A -> B -> C -> B -> ...
+#
+# Write a method cyclic?(first_link), which will return true if a list is cyclic. Your first version may use O(n) memory. Next, write a version which uses O(1) memory; you'll probably need a different approach.
+
+def cyclic?(first_link)
+  seen = {}
+  seen[first_link] = true
+
+  current_link = first_link
+  until current_link.next == nil
+    current_link = current_link.next
+    if seen[current_link]
+      return true
+    else
+      seen[current_link] = true
+    end
+  end
+
+  false
+end
+
+def cyclic?(first_link)
+  slow_link = first_link
+  fast_link = first_link
+
+  until fast_link == nil || slow_link == nil
+    slow_link = slow_link.next
+    fast_link = fast_link.next.next
+    return true if slow_link == fast_link
+  end
+
+  false
+end
+
+# Given two singly-linked lists of (possibly) differing lengths that converge at some point, find the node at which they converge.
+
+# Imagine two runners, running at the same speed across the linked lists. If one list is longer than the other, the shorter list's runner will reach the end first. The difference between the two runners' times is the difference in length between the two lists. Knowing this, we can give one runner a head-start of a distance equivalent to the difference in length between the two lists. Then traversing both lists at equal speed will result in the two runners colliding at the intersection point.
+
+# **************************************************************************** #
