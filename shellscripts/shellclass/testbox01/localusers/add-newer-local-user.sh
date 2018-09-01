65664c8ab9de07d710d9be217968d998caa82810
Ed Huang <edhuang24@gmail.com>
2018-09-01 13:25:49 -0500

    add third exercise

--- shellscripts/shellclass/testbox01/localusers/add-newer-local-user.sh
+++ shellscripts/shellclass/testbox01/localusers/add-newer-local-user.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
+#
 
 # Make sure the script is being executed with superuser privileges.
-if [[ ${UID} != 0 ]]
+if [[ "${UID}" != 0 ]]
 then
-  echo "Please login with root privileges"
+  echo "Please use superuser privileges" >&2
   exit 1
 fi
 
 # If the user doesn't supply at least one argument, then give them help.
-if [[ ${#} < 1 ]]
+if [[ "${#}" < 1 ]]
 then
-  echo "Please supply at least one argument"
+  echo "You must supply at least one argument" >&2
   exit 1
 fi
 
 # The first parameter is the user name.
-USER_NAME=${1}
+USER_NAME="${1}"
 
 # The rest of the parameters are for the account comments.
 shift
-COMMENT=${@}
+COMMENTS="${@}"
 
 # Generate a password.
-PASSWORD=$(date +%s | head -c5)
+PASSWORD=`date +%s | head -c5`
 
 # Create the user with the password.
-useradd -c ${COMMENT} -m ${USER_NAME}
+useradd -c "${COMMENTS}" -m "${USER_NAME}" &> /dev/null
 
 # Check to see if the useradd command succeeded.
-if [[ ${?} != 0 ]]
+if [[ "${?}" != 0 ]]
 then
-  "the previous command did not work"
+  echo "Your command did not succeed, please try again" >&2
   exit 1
 fi
 
 # Set the password.
-echo #{PASSWORD} | passwd --stdin ${USER_NAME}
+echo PASSWORD | passwd -stdin ${USER_NAME} &> /dev/null
 
 # Check to see if the passwd command succeeded.
-if [[ ${?} != 0 ]]
+if [[ "${?}" != 0 ]]
 then
-  "the previous command did not work"
+  echo "Your command did not succeed, please try again" >&2
   exit 1
 fi
 
 # Force password change on first login.
-passwd -e ${USER_NAME}
+passwd -e ${USER_NAME} &> /dev/null
 
 # Display the username, password, and the host where the user was created.
-echo
 echo 'username:'
 echo "${USER_NAME}"
 echo
