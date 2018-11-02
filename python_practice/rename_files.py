a706791f1b47cff878d562a0ffaa40885fec2f4a
edhuang24 <edhuang24@gmail.com>
2018-11-02 12:14:02 -0700

    finish lession 1

---
+++ python_practice/rename_files.py
@@ -0,0 +1,15 @@
+import os
+
+def rename_files():
+    file_list = os.listdir("/Users/ehuang/Desktop/Algorithms-DataStructures/python_practice/prank")
+    # print(file_list)
+    saved_path = os.getcwd()
+    # print(saved_path)
+    os.chdir("/Users/ehuang/Desktop/Algorithms-DataStructures/python_practice/prank")
+
+    for file_name in file_list:
+        # os.rename(file_name, file_name[2:])
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
+
+rename_files()
---
+++ python_practice/udacity.py
@@ -0,0 +1,8 @@
+import time
+import webbrowser
+
+count = 0
+while (count < 3):
+    time.sleep(10)
+    webbrowser.open("https://www.forbes.com/sites/jessecolombo/2018/08/29/the-u-s-is-experiencing-a-dangerous-corporate-debt-bubble/#469196bb600e")
+    count += 1
