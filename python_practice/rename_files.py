5e101853e0a89f72eb7ca08b26f2aedda5f78405
edhuang24 <edhuang24@gmail.com>
2018-11-02 12:21:42 -0700

    add print statements

--- python_practice/rename_files.py
+++ python_practice/rename_files.py
@@ -8,6 +8,8 @@ def rename_files():
     os.chdir("/Users/ehuang/Desktop/Algorithms-DataStructures/python_practice/prank")
 
     for file_name in file_list:
+        print("Old Name - "+file_name)
+        print("New Name - "+file_name.translate(None, "0123456789"))
         # os.rename(file_name, file_name[2:])
         os.rename(file_name, file_name.translate(None, "0123456789"))
     os.chdir(saved_path)
